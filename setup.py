79f72f575d4255a3df7f147f82661ed8f9999019
Brian Muller <bamuller@gmail.com>
2013-05-13 17:45:08 -0400

    bumped version to 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="txairbrake",
-    version="0.1",
+    version="0.2",
     description="txairbrake reports exceptions in Twisted code to an airbrake server",
     author="Brian Muller",
     author_email="bamuller@gmail.com",
